6a755d695c7c5d4bed9d5a6cf6b32fde0aa5cd53
Luke Spragg <luke@spragg.me>
2018-01-05 14:25:25 -0500

    Some improvements and tweaks

--- _plugins/humanize.rb
+++ _plugins/humanize.rb
@@ -1,7 +1,16 @@
 class String
   def humanize
-    # TODO: Improve this to not require .strip
-    self.gsub(/((?<=[a-z])[A-Z]|[A-Z](?=[a-z]))/, ' \0').strip
+    # Split PascalCase on upper cases after a lowercase character (ex. WelcomeTP)
+    self.gsub(/([[:lower:]\\d])([[:upper:]])/, '\1 \2') \
+    # Split PascalCase on first capital after two or more capitals (ex. NPCManager)
+    .gsub(/([[:upper:]]{2,})([[:upper:]][[:lower:]\\d])/,'\1 \2') \
+    # Split before a number with multiple characters in front of it (ex. Test99Tool)
+    .gsub(/(\D{2,})(\d+)/, '\1 \2') \
+    # Split PascalCase after a number (ex. C4Logger)
+    .gsub(/(\d)/, '\1 \2') \
+    # Remove any whitespace from start or end of string
+    .strip
+
     # TODO: Add support for other string formats and small words (ie. and, to, or, of)
   end
 end
