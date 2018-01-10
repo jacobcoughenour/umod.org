467e3c1c7db704a6dc31eb8906a1ee1294cb6e8f
Luke Spragg <luke@spragg.me>
2018-01-10 02:32:56 -0500

    Some https URL changes

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
     // Start writing your custom functions here.
-    // All the necessary pluigns are already loaded.
+    // All the necessary plugins are already loaded.
 });
