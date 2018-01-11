7e1b4bad56b8bc7d177a6da621778cd25aaa0400
Luke Spragg <luke@spragg.me>
2018-01-10 23:10:07 -0500

    Add anchors to plugin page headings

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
-    // Start writing your custom functions here.
-    // All the necessary plugins are already loaded.
+    // Write any custom functions here. All the necessary plugins are already loaded.
+
+    $('h2,h3,h4,h5,h6').filter('[id]').each(function () {
+        $(this).html('<a href="#' + $(this).attr('id') + '" class="fragment-identifier js-fragment-identifier fragment-identifier-scroll"><i class="fa fa-link"></i></a>'+ $(this).text());
+    });
 });
