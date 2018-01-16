10b96bb2a5473d3761ced863d95f0dd478cff89e
Luke Spragg <luke@spragg.me>
2018-01-15 22:47:47 -0500

    Fix plugins.json file location

--- _plugins/plugin_pages.rb
+++ _plugins/plugin_pages.rb
@@ -179,7 +179,6 @@ module Jekyll
     # Gets the URL for a specific file from a GitHub repository
     def get_contents_url(contents, filename)
       contents.each do |content|
-        puts "Getting content URL for #{filename}"
         return content['download_url'] if content['filename'] == filename
       end
       nil
@@ -259,7 +258,7 @@ module Jekyll
       self.pages << index
 
       # Write the plugins/index.json file
-      write_static_file(plugins.to_json, 'plugins.json', '')
+      write_static_file(plugins.to_json, 'plugins.json', '/')
     end
 
     # Loops through the list of plugin and processes each one
