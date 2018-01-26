57c227215ecbd6fc78c7ae121af6c33bc33023f5
Jacob Coughenour <jacob15545@yahoo.com>
2018-01-25 21:57:44 -0500

    Add scroll arrows and mouse dragging threshold

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -10,36 +10,43 @@ $(document).ready(function() {
 
 function AttachHorizontalScroll(element) {
     $("ul", element)
-        .data('shadowLeft', $("<div>").addClass("scroll-shadow-left").insertBefore($("ul", element)))
-        .data('shadowRight', $("<div>").addClass("scroll-shadow-right").insertAfter($("ul", element)))
-    .mousedown(function (e) {
-        if (this.scrollWidth > this.offsetWidth)
-            $(this).data('down', true).data('x', e.clientX).data('scrollLeft', this.scrollLeft);
-        return false;
-    }).mouseup(function (e) {
-        $(this).data('down', false).removeClass("dragging");
-    }).mouseleave(function (e) {
-        $(this).data('down', false).removeClass("dragging");
-    }).mousemove(function (e) {
-        if ($(this).data('down') == true) {
-            $(this).toggleClass("dragging", true);
-            this.scrollLeft = $(this).data('scrollLeft') + $(this).data('x') - e.clientX;
-            $(this).trigger("UpdateShadows");
-        }
-    }).mousewheel(function (e, delta) {
-        if (this.scrollWidth > this.offsetWidth) {
-            this.scrollLeft -= (delta * 32);
-            e.preventDefault();
-            $(this).trigger("UpdateShadows");
+        .data('shadowLeft', $("<a>").addClass("scroll-shadow-left").insertBefore($("ul", element)).html('<i class="fa fa-chevron-left" aria-hidden="true"></i>').mousedown(function (e) {
+            let ul = $("ul", this.parentNode);
+            ul.animate({ scrollLeft: "-=" + (ul.width() * 0.5) }, { step: (n, fx) => { $(fx.elem).trigger("UpdateShadows"); } });
+        }))
+        .data('shadowRight', $("<a>").addClass("scroll-shadow-right").insertAfter($("ul", element)).html('<i class="fa fa-chevron-right" aria-hidden="true"></i>').mousedown(function (e) {
+            let ul = $("ul", this.parentNode);
+            ul.animate({ scrollLeft: "+=" + (ul.width() * 0.5) }, { step: (n, fx) => { $(fx.elem).trigger("UpdateShadows"); } });
+        }))
+        .mousedown(function (e) {
+            if (this.scrollWidth > this.offsetWidth)
+                $(this).data('down', true).data('x', e.clientX).data('scrollLeft', this.scrollLeft);
             return false;
-        }
-    }).on("UpdateShadows", e => {
-        if (e.target.scrollWidth > e.target.offsetWidth) {
-            $(e.target).data('shadowLeft').css("width", Math.min(e.target.scrollLeft, 50) + 'px');
-            $(e.target).data('shadowRight').css("width", Math.min(e.target.scrollWidth - e.target.scrollLeft - e.target.offsetWidth, 50) + 'px');
-        } else {
-            $(e.target).data('shadowLeft').css("width", '0px');
-            $(e.target).data('shadowRight').css("width", '0px');
-        }
-    }).css({ "overflow": "hidden" }).trigger("UpdateShadows");
+        }).mouseup(function (e) {
+            $(this).data('down', false).removeClass("dragging");
+        }).mouseleave(function (e) {
+            $(this).data('down', false).removeClass("dragging");
+        }).mousemove(function (e) {
+            let dist = $(this).data('x') - e.clientX;
+            if ($(this).data('down') == true && (Math.abs(dist) > 16 || $(this).hasClass("dragging"))) {
+                $(this).toggleClass("dragging", true);
+                this.scrollLeft = $(this).data('scrollLeft') + dist;
+                $(this).trigger("UpdateShadows");
+            }
+        }).mousewheel(function (e, delta) {
+            if (this.scrollWidth > this.offsetWidth) {
+                this.scrollLeft -= (delta * 32);
+                e.preventDefault();
+                $(this).trigger("UpdateShadows");
+                return false;
+            }
+        }).on("UpdateShadows", e => {
+            if (e.target.scrollWidth > e.target.offsetWidth) {
+                $(e.target).data('shadowLeft').css("width", Math.min(e.target.scrollLeft, 50) + 'px');
+                $(e.target).data('shadowRight').css("width", Math.min(e.target.scrollWidth - e.target.scrollLeft - e.target.offsetWidth, 50) + 'px');
+            } else {
+                $(e.target).data('shadowLeft').css("width", '0px');
+                $(e.target).data('shadowRight').css("width", '0px');
+            }
+        }).css({ "overflow": "hidden" }).trigger("UpdateShadows");
 }
