f3d95e76a78d24a552d35f2e203c4914ff2e0536
TheGreatJ <jacob15545@yahoo.com>
2018-03-06 11:48:21 -0500

    Store sortby, view, and itemsperpage in localStorage

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -60,3 +60,14 @@ function AttachHorizontalScroll(element) {
             }
         }).css({ "overflow": "hidden" }).trigger("UpdateShadows");
 }
+
+function storageAvailable(type) {
+    try {
+        var storage = window[type], x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    } catch(e) {
+        return e instanceof DOMException && (e.code === 22 || e.code === 1014 || e.name === 'QuotaExceededError' || e.name === 'NS_ERROR_DOM_QUOTA_REACHED') && storage.length !== 0;
+    }
+}
